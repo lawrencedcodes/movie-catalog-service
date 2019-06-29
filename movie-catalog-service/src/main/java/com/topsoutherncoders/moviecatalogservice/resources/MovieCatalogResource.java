849eb8da08216f4021dfa97f1e8c47af2ee08a4e
Lawrence Lockhart <ldlockhart@gmail.com>
2019-06-29 16:46:04 -0500

    Add controllers to handle userID parameter input

--- movie-catalog-service/src/main/java/com/topsoutherncoders/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/topsoutherncoders/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,4 +1,23 @@
 package com.topsoutherncoders.moviecatalogservice.resources;
 
+import com.sun.org.apache.xml.internal.resolver.Catalog;
+import com.topsoutherncoders.moviecatalogservice.models.CatalogItem;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("/catalog")
 public class MovieCatalogResource {
+
+    @RequestMapping("/{userID}")
+    public List<CatalogItem> getCatalog(@PathVariable("userID") String userID) {
+
+        return Collections.singletonList(
+                new CatalogItem("Avengers","Action",4)
+        );
+    }
 }
