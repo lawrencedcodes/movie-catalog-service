613373ae72d9d37d0a929b90a218b08d5d10fc7a
Lawrence Lockhart <ldlockhart@gmail.com>
2019-06-29 17:08:50 -0500

    Add routing for movie info service

---
+++ movie-info-service/src/main/java/com/topsoutherncoders/movieinfoservice/models/Movie.java
@@ -0,0 +1,27 @@
+package com.topsoutherncoders.movieinfoservice.models;
+
+public class Movie {
+    public Movie(String movieID, String name) {
+        this.movieID = movieID;
+        this.name = name;
+    }
+
+    private String movieID;
+    private String name;
+
+    public String getMovieID() {
+        return movieID;
+    }
+
+    public void setMovieID(String movieID) {
+        this.movieID = movieID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
