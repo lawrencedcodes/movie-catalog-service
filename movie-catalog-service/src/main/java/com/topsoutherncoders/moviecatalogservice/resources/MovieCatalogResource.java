17ffe99767cb6638690d5e11bc4691f4fded58e0
Lawrence Lockhart <ldlockhart@gmail.com>
2019-06-30 22:07:08 -0500

    hardcore ratings, return object

--- movie-catalog-service/src/main/java/com/topsoutherncoders/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/topsoutherncoders/moviecatalogservice/resources/MovieCatalogResource.java
@@ -2,12 +2,15 @@ package com.topsoutherncoders.moviecatalogservice.resources;
 
 import com.sun.org.apache.xml.internal.resolver.Catalog;
 import com.topsoutherncoders.moviecatalogservice.models.CatalogItem;
+import com.topsoutherncoders.moviecatalogservice.models.Rating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
@@ -16,8 +19,17 @@ public class MovieCatalogResource {
     @RequestMapping("/{userID}")
     public List<CatalogItem> getCatalog(@PathVariable("userID") String userID) {
 
-        return Collections.singletonList(
-                new CatalogItem("Avengers","Action",4)
+        List<Rating> ratings = Arrays.asList(
+                new Rating("1234",4),
+                new Rating("1235",3),
+                new Rating("1236",5)
         );
+
+        return ratings.stream().map(rating -> new CatalogItem("Avengers","Action",4));
+            .collect(Collectors.toList());
+        //get all rated movie IDs
+        //for each movie ID, call movie info service and get details
+        //put them all together
+
     }
 }
